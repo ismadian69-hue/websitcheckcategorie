12f09620c59424eaa9f0e55bcb67c462a61828e6
ismadian69-hue <ismadian69@gmail.com>
2026-04-27 14:29:29 +0100

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,12 +1,5 @@
-export default async function handler(req, res) {
-try {
+export default function handler(req,res){
 res.status(200).json({
-keyExists: !!process.env.VT_API_KEY,
-keyStart: process.env.VT_API_KEY?.slice(0,6) || null
+keyExists: !!process.env.VT_API_KEY
 });
-} catch {
-res.status(200).json({
-keyExists:false
-});
-}
 }
